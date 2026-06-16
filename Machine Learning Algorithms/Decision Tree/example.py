8cd21e688b664e5ddda61d88adee1b7a348345a4
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-16 20:08:52 +0530

    Create example.py

---
+++ Machine Learning Algorithms/Decision Tree/example.py
@@ -0,0 +1,35 @@
+```python
+import numpy as np
+from decision_tree import DecisionTree
+
+X = np.array([
+    [2, 3],
+    [3, 4],
+    [4, 5],
+    [8, 7],
+    [9, 8],
+    [10, 9]
+])
+
+y = np.array([
+    0,
+    0,
+    0,
+    1,
+    1,
+    1
+])
+
+model = DecisionTree(max_depth=3)
+
+model.fit(X, y)
+
+predictions = model.predict(
+    np.array([
+        [3, 3],
+        [9, 9]
+    ])
+)
+
+print("Predictions:", predictions)
+```
