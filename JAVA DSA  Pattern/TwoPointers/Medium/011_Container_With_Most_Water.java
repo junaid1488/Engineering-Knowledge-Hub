5f793067b037adacaa0d5ec1577a300e0b98c38d
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-18 20:12:09 +0530

    Create 011_Container_With_Most_Water.java

---
+++ JAVA DSA  Pattern/TwoPointers/Medium/011_Container_With_Most_Water.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int maxArea = 0;
+
+        while (left < right) {
+
+            int width = right - left;
+
+            int area = width * Math.min(
+                height[left],
+                height[right]
+            );
+
+            maxArea = Math.max(
+                maxArea,
+                area
+            );
+
+            if (
+                height[left] <
+                height[right]
+            ) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
