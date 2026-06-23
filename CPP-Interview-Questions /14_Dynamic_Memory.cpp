ac4327ddf9856b8596994704266503fe73e6a8e6
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-23 20:31:47 +0530

    Add dynamic memory allocation example in C++

---
+++ CPP-Interview-Questions /14_Dynamic_Memory.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int* arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = i + 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+
+    return 0;
+}
