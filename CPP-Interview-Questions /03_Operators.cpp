03024716dce378d35a2d7da3263f23b86876966b
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-23 20:18:34 +0530

    Create 03_Operators.cpp

---
+++ CPP-Interview-Questions /03_Operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 15, b = 4;
+
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b << '\n';
+    cout << a % b << '\n';
+
+    cout << (a > b) << '\n';
+    cout << (a == b) << '\n';
+
+    return 0;
+}
