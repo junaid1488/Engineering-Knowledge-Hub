f628412d1647bce91854d5320c5604008bda2f52
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-24 20:46:25 +0530

    Add hybrid inheritance example in C++
    Implemented hybrid inheritance with virtual base class.

--- CPP-Interview-Questions /30_Hybrid_Inheritance.cpp
+++ CPP-Interview-Questions /30_Hybrid_Inheritance.cpp
@@ -1,3 +1,23 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Hybrid Inheritance
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void show() {
+        cout << "Base Class" << endl;
+    }
+};
+
+class B : virtual public A {};
+class C : virtual public A {};
+
+class D : public B, public C {};
+
+int main() {
+    D obj;
+    obj.show();
+    return 0;
+}
