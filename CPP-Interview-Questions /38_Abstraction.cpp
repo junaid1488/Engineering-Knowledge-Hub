3c1e868a3d2762b91ddccb54c7e9f116aeaca65e
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-24 20:49:08 +0530

    Implement ATM abstraction with SBIATM class

--- CPP-Interview-Questions /38_Abstraction.cpp
+++ CPP-Interview-Questions /38_Abstraction.cpp
@@ -1,3 +1,23 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Abstraction
+#include <iostream>
+using namespace std;
+
+class ATM {
+public:
+    virtual void withdraw() = 0;
+};
+
+class SBIATM : public ATM {
+public:
+    void withdraw() override {
+        cout << "Money Withdrawn" << endl;
+    }
+};
+
+int main() {
+    SBIATM atm;
+    atm.withdraw();
+    return 0;
+}
