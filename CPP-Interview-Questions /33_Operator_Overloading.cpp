58ca7d7fb64353c8dc0ebab4333dcd844a0c82d9
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-24 20:47:29 +0530

    Implement operator overloading for Complex class

--- CPP-Interview-Questions /33_Operator_Overloading.cpp
+++ CPP-Interview-Questions /33_Operator_Overloading.cpp
@@ -1,3 +1,27 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Operator Overloading
+#include <iostream>
+using namespace std;
+
+class Complex {
+public:
+    int real, imag;
+
+    Complex(int r = 0, int i = 0) : real(r), imag(i) {}
+
+    Complex operator+(const Complex& obj) {
+        return Complex(real + obj.real, imag + obj.imag);
+    }
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(2, 3), c2(4, 5);
+    Complex c3 = c1 + c2;
+    c3.display();
+    return 0;
+}
