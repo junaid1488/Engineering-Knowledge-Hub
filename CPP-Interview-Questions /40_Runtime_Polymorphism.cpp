410e626984f67e19827feefd4d7662cd852fe860
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-24 20:49:50 +0530

    Implement runtime polymorphism with Animal and Dog classes

--- CPP-Interview-Questions /40_Runtime_Polymorphism.cpp
+++ CPP-Interview-Questions /40_Runtime_Polymorphism.cpp
@@ -1,3 +1,27 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Runtime Polymorphism
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal Sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() override {
+        cout << "Dog Barking" << endl;
+    }
+};
+
+int main() {
+    Animal* ptr;
+    Dog d;
+    ptr = &d;
+    ptr->sound();
+    return 0;
+}
