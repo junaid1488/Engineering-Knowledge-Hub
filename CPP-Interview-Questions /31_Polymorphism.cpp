09d72b0dbe12999bcca2dcadb67ca97699e4efb9
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-24 20:46:43 +0530

    Implement function overloading in Print class

--- CPP-Interview-Questions /31_Polymorphism.cpp
+++ CPP-Interview-Questions /31_Polymorphism.cpp
@@ -1,3 +1,23 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Polymorphism
+#include <iostream>
+using namespace std;
+
+class Print {
+public:
+    void show(int x) {
+        cout << x << endl;
+    }
+
+    void show(double x) {
+        cout << x << endl;
+    }
+};
+
+int main() {
+    Print p;
+    p.show(10);
+    p.show(10.5);
+    return 0;
+}
