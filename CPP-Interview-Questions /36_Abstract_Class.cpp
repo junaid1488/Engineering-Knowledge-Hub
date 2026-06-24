7b145583a833d7233de484bdbd451195243eb68a
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-24 20:48:27 +0530

    Implement abstract class with Dog subclass

--- CPP-Interview-Questions /36_Abstract_Class.cpp
+++ CPP-Interview-Questions /36_Abstract_Class.cpp
@@ -1,3 +1,23 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Abstract Class
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() = 0;
+};
+
+class Dog : public Animal {
+public:
+    void sound() override {
+        cout << "Bark" << endl;
+    }
+};
+
+int main() {
+    Dog d;
+    d.sound();
+    return 0;
+}
