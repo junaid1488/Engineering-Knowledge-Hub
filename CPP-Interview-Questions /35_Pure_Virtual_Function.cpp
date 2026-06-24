e29d6062b1010a9828c05b4b809bb442f489515b
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-24 20:48:07 +0530

    Implement pure virtual function example in C++

--- CPP-Interview-Questions /35_Pure_Virtual_Function.cpp
+++ CPP-Interview-Questions /35_Pure_Virtual_Function.cpp
@@ -1,3 +1,23 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Pure Virtual Function
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void draw() = 0;
+};
+
+class Circle : public Shape {
+public:
+    void draw() override {
+        cout << "Drawing Circle" << endl;
+    }
+};
+
+int main() {
+    Circle c;
+    c.draw();
+    return 0;
+}
