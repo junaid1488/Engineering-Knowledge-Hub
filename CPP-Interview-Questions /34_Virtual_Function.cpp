7185365e7d247f283a8ce61addb8ed8b57e61734
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-24 20:47:47 +0530

    Implement virtual function example with Base and Derived

--- CPP-Interview-Questions /34_Virtual_Function.cpp
+++ CPP-Interview-Questions /34_Virtual_Function.cpp
@@ -1,3 +1,27 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Virtual Function
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    virtual void show() {
+        cout << "Base Class" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    void show() override {
+        cout << "Derived Class" << endl;
+    }
+};
+
+int main() {
+    Base* ptr;
+    Derived d;
+    ptr = &d;
+    ptr->show();
+    return 0;
+}
