b15760680176382092c37c50ccddc90007742db4
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:41:33 +0530

    Add Binary Search Tree implementation
    Implemented a Binary Search Tree with insert and inorder traversal functions.

--- CPP-Interview-Questions /57_BST.cpp
+++ CPP-Interview-Questions /57_BST.cpp
@@ -4,3 +4,51 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Binary Search Tree (BST)
 */
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+Node* insert(Node* root, int key) {
+    if (!root)
+        return new Node(key);
+
+    if (key < root->data)
+        root->left = insert(root->left, key);
+    else
+        root->right = insert(root->right, key);
+
+    return root;
+}
+
+void inorder(Node* root) {
+    if (!root)
+        return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Node* root = nullptr;
+
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+
+    inorder(root);
+
+    return 0;
+}
