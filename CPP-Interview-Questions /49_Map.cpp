e1c39d4ced4f062ebd6d7734882d8338a9988a4a
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:37:44 +0530

    Implement basic map usage example in C++

--- CPP-Interview-Questions /49_Map.cpp
+++ CPP-Interview-Questions /49_Map.cpp
@@ -4,3 +4,19 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Map (STL)
 */
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    map<int, string> mp;
+
+    mp[101] = "Alice";
+    mp[102] = "Bob";
+    mp[103] = "Charlie";
+
+    for (auto it : mp)
+        cout << it.first << " " << it.second << endl;
+
+    return 0;
+}
