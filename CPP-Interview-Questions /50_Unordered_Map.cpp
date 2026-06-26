fcdb797d56be745fc5a70c0283e914025e06aab1
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:38:00 +0530

    Implement unordered_map example in C++
    Added a sample program demonstrating the use of unordered_map in C++.

--- CPP-Interview-Questions /50_Unordered_Map.cpp
+++ CPP-Interview-Questions /50_Unordered_Map.cpp
@@ -4,3 +4,19 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Unordered Map (STL)
 */
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    unordered_map<int, string> ump;
+
+    ump[1] = "Apple";
+    ump[2] = "Banana";
+    ump[3] = "Mango";
+
+    for (auto it : ump)
+        cout << it.first << " " << it.second << endl;
+
+    return 0;
+}
