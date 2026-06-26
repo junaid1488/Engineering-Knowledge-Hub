d424cf0093eaa67cf23021ec056e58fc36640cac
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:41:17 +0530

    Add binary tree implementation and inorder function
    Implement binary tree with inorder traversal

--- CPP-Interview-Questions /56_Binary_Tree.cpp
+++ CPP-Interview-Questions /56_Binary_Tree.cpp
@@ -4,3 +4,37 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Binary Tree
 */
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+void inorder(Node* root) {
+    if (!root)
+        return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    inorder(root);
+
+    return 0;
+}
