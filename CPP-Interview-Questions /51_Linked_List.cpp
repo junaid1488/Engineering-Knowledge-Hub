6399cc5fcbfe1787dd5b13f1a39440d588870857
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:39:39 +0530

    Add basic singly linked list implementation
    Implement a simple singly linked list with a main function to display node values.

--- CPP-Interview-Questions /51_Linked_List.cpp
+++ CPP-Interview-Questions /51_Linked_List.cpp
@@ -4,3 +4,31 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Singly Linked List
 */
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+int main() {
+    Node* head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+
+    Node* temp = head;
+
+    while (temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
