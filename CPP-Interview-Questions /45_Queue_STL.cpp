d325529f461ff8301ecd7ec4b248e011dc9250e4
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:36:09 +0530

    Implement basic queue operations in C++
    Demonstrate basic queue operations using STL.

--- CPP-Interview-Questions /45_Queue_STL.cpp
+++ CPP-Interview-Questions /45_Queue_STL.cpp
@@ -4,3 +4,25 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Queue (STL)
 */
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    queue<int> q;
+
+    q.push(10);
+    q.push(20);
+    q.push(30);
+
+    cout << "Front: " << q.front() << endl;
+    cout << "Back: " << q.back() << endl;
+
+    q.pop();
+
+    cout << "After Pop Front: " << q.front() << endl;
+
+    cout << "Size: " << q.size() << endl;
+
+    return 0;
+}
