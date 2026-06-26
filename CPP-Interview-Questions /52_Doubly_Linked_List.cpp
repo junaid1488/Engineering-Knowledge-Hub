241d67e5f8142ebdc22750f348beebcb0b52e64e
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:40:00 +0530

    Implement basic doubly linked list in C++
    Added a simple implementation of a doubly linked list with node creation and traversal.

--- CPP-Interview-Questions /52_Doubly_Linked_List.cpp
+++ CPP-Interview-Questions /52_Doubly_Linked_List.cpp
@@ -4,3 +4,36 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Doubly Linked List
 */
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *prev, *next;
+
+    Node(int val) {
+        data = val;
+        prev = next = nullptr;
+    }
+};
+
+int main() {
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+
+    head->next = second;
+    second->prev = head;
+    second->next = third;
+    third->prev = second;
+
+    Node* temp = head;
+
+    while (temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
