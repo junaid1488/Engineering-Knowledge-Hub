5a663ca4d050fad7f7fc9447ed842d63bff20955
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:40:56 +0530

    Add Queue implementation in C++
    Implemented a basic queue class with push, pop, and front methods.

--- CPP-Interview-Questions /55_Queue_Implementation.cpp
+++ CPP-Interview-Questions /55_Queue_Implementation.cpp
@@ -4,3 +4,47 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Queue Implementation
 */
+#include <iostream>
+using namespace std;
+
+class Queue {
+    int arr[100];
+    int front, rear;
+
+public:
+    Queue() {
+        front = rear = 0;
+    }
+
+    void push(int x) {
+        arr[rear++] = x;
+    }
+
+    void pop() {
+        if (front < rear)
+            front++;
+    }
+
+    int Front() {
+        return arr[front];
+    }
+
+    bool empty() {
+        return front == rear;
+    }
+};
+
+int main() {
+    Queue q;
+
+    q.push(10);
+    q.push(20);
+    q.push(30);
+
+    while (!q.empty()) {
+        cout << q.Front() << " ";
+        q.pop();
+    }
+
+    return 0;
+}
