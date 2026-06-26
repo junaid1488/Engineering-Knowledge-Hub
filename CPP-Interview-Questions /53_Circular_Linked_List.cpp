2f8f4d747a340fe062be8cc11fc16ae32148f52e
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:40:19 +0530

    Add circular linked list implementation in C++
    Implement a circular linked list with basic node structure and traversal.

--- CPP-Interview-Questions /53_Circular_Linked_List.cpp
+++ CPP-Interview-Questions /53_Circular_Linked_List.cpp
@@ -4,3 +4,35 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Circular Linked List
 */
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+int main() {
+    Node* first = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+
+    first->next = second;
+    second->next = third;
+    third->next = first;
+
+    Node* temp = first;
+
+    do {
+        cout << temp->data << " ";
+        temp = temp->next;
+    } while (temp != first);
+
+    return 0;
+}
