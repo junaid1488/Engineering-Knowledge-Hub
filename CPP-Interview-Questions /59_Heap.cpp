aa414be8ea2c9efb58a598ff328701ffbe5894d9
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:46:17 +0530

    Implement max heap using priority_queue in C++
    Added a simple implementation of a max heap using priority_queue.

--- CPP-Interview-Questions /59_Heap.cpp
+++ CPP-Interview-Questions /59_Heap.cpp
@@ -4,3 +4,18 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Heap
 */
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    priority_queue<int> heap;
+
+    for (int x : {40, 20, 50, 10, 30})
+        heap.push(x);
+
+    while (!heap.empty()) {
+        cout << heap.top() << " ";
+        heap.pop();
+    }
+}
