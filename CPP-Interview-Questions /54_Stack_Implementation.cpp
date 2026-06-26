c0e521be59a32a615ff08e8438e70b9ea281c762
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-26 15:40:36 +0530

    Add stack implementation in C++
    Implemented a stack class with push, pop, peek, and empty methods. Demonstrated stack usage in the main function.

--- CPP-Interview-Questions /54_Stack_Implementation.cpp
+++ CPP-Interview-Questions /54_Stack_Implementation.cpp
@@ -4,3 +4,47 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Stack Implementation
 */
+#include <iostream>
+using namespace std;
+
+class Stack {
+    int arr[100];
+    int top;
+
+public:
+    Stack() {
+        top = -1;
+    }
+
+    void push(int x) {
+        arr[++top] = x;
+    }
+
+    void pop() {
+        if (top >= 0)
+            top--;
+    }
+
+    int peek() {
+        return arr[top];
+    }
+
+    bool empty() {
+        return top == -1;
+    }
+};
+
+int main() {
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    while (!s.empty()) {
+        cout << s.peek() << " ";
+        s.pop();
+    }
+
+    return 0;
+}
