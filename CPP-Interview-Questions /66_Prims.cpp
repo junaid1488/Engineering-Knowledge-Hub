2396f83d38391de5e279fad2356b05541e31ed9c
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-27 17:46:24 +0530

    Enhance comments in Prim's Algorithm implementation
    Added comments to explain the functionality of the Prim's Algorithm implementation.

--- CPP-Interview-Questions /66_Prims.cpp
+++ CPP-Interview-Questions /66_Prims.cpp
@@ -4,3 +4,8 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Prim's Algorithm
 */
+// Represents the graph using an adjacency list.
+// Uses a priority queue to select the minimum weight edge.
+// Maintains visited vertices.
+// Builds the Minimum Spanning Tree incrementally.
+// Displays the total weight of the Minimum Spanning Tree.
