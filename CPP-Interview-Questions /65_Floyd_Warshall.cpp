35e0c0b800b3c30b2e2bb1f7d851e0a95b7b405e
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-27 17:46:06 +0530

    Enhance comments in Floyd-Warshall.cpp
    Added comments to explain the Floyd-Warshall algorithm implementation.

--- CPP-Interview-Questions /65_Floyd_Warshall.cpp
+++ CPP-Interview-Questions /65_Floyd_Warshall.cpp
@@ -4,3 +4,8 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Floyd-Warshall Algorithm
 */
+// Stores graph using an adjacency matrix.
+// Initializes the distance matrix.
+// Updates shortest paths using every vertex as an intermediate node.
+// Computes shortest paths between all vertex pairs.
+// Displays the final distance matrix.
