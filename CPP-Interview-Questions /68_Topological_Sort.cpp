d75285b72fa9878917df56d7b2cd2cee63dddba2
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-27 17:52:51 +0530

    Add topological sort implementation in C++
    Implement topological sort using Kahn's algorithm.

--- CPP-Interview-Questions /68_Topological_Sort.cpp
+++ CPP-Interview-Questions /68_Topological_Sort.cpp
@@ -9,3 +9,42 @@ Topic     : Topological Sort
 // Uses a queue to process vertices with zero indegree.
 // Generates the Topological Ordering.
 // Displays the final topological sequence.
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    int V = 6;
+
+    vector<vector<int>> adj(V);
+    vector<int> indegree(V,0);
+
+    adj[5]={2,0};
+    adj[4]={0,1};
+    adj[2]={3};
+    adj[3]={1};
+
+    for(int i=0;i<V;i++)
+        for(int v:adj[i])
+            indegree[v]++;
+
+    queue<int> q;
+
+    for(int i=0;i<V;i++)
+        if(indegree[i]==0)
+            q.push(i);
+
+    while(!q.empty()){
+        int u=q.front();
+        q.pop();
+
+        cout<<u<<" ";
+
+        for(int v:adj[u]){
+            indegree[v]--;
+            if(indegree[v]==0)
+                q.push(v);
+        }
+    }
+}
