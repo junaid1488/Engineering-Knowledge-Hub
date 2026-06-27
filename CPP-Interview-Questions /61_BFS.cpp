5e73d491a620e631014ce4375634f2a8490e8460
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-27 17:49:57 +0530

    Add BFS implementation in 61_BFS.cpp
    Implement BFS algorithm using a queue to traverse a graph.

--- CPP-Interview-Questions /61_BFS.cpp
+++ CPP-Interview-Questions /61_BFS.cpp
@@ -9,3 +9,36 @@ Topic     : Breadth First Search (BFS)
 // Marks visited vertices to avoid revisiting.
 // Traverses all reachable vertices level by level.
 // Displays the BFS traversal sequence.
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    int V = 5;
+    vector<vector<int>> adj(V);
+
+    adj[0] = {1, 2};
+    adj[1] = {3};
+    adj[2] = {4};
+
+    vector<bool> vis(V, false);
+    queue<int> q;
+
+    q.push(0);
+    vis[0] = true;
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        cout << u << " ";
+
+        for (int v : adj[u]) {
+            if (!vis[v]) {
+                vis[v] = true;
+                q.push(v);
+            }
+        }
+    }
+}
