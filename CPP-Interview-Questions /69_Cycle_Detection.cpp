282d51a251179540402ed83714527d9dbaa0b3f5
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-27 17:53:06 +0530

    Implement cycle detection using DFS in a graph

--- CPP-Interview-Questions /69_Cycle_Detection.cpp
+++ CPP-Interview-Questions /69_Cycle_Detection.cpp
@@ -9,3 +9,39 @@ Topic     : Cycle Detection in Graph
 // Tracks visited and recursion stack states.
 // Detects whether a cycle exists in the graph.
 // Displays the cycle detection result.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool dfs(int u, vector<vector<int>>& adj, vector<int>& vis) {
+    vis[u] = 1;
+
+    for (int v : adj[u]) {
+        if (vis[v] == 1)
+            return true;
+
+        if (vis[v] == 0 && dfs(v, adj, vis))
+            return true;
+    }
+
+    vis[u] = 2;
+    return false;
+}
+
+int main() {
+    int V = 4;
+
+    vector<vector<int>> adj(V);
+
+    adj[0]={1};
+    adj[1]={2};
+    adj[2]={3};
+    adj[3]={1};
+
+    vector<int> vis(V,0);
+
+    if(dfs(0,adj,vis))
+        cout<<"Cycle Detected";
+    else
+        cout<<"No Cycle";
+}
