091b576113672b045a1817fdb58d24dd78d87750
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-27 17:50:17 +0530

    Add DFS implementation with example usage
    Implement Depth First Search (DFS) algorithm using recursion. The code includes a main function to demonstrate DFS on a sample graph.

--- CPP-Interview-Questions /62_DFS.cpp
+++ CPP-Interview-Questions /62_DFS.cpp
@@ -9,3 +9,28 @@ Topic     : Depth First Search (DFS)
 // Marks visited vertices to prevent repeated traversal.
 // Visits each connected vertex recursively.
 // Displays the DFS traversal sequence.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void dfs(int u, vector<vector<int>>& adj, vector<bool>& vis) {
+    vis[u] = true;
+    cout << u << " ";
+
+    for (int v : adj[u])
+        if (!vis[v])
+            dfs(v, adj, vis);
+}
+
+int main() {
+    int V = 5;
+    vector<vector<int>> adj(V);
+
+    adj[0] = {1, 2};
+    adj[1] = {3};
+    adj[2] = {4};
+
+    vector<bool> vis(V, false);
+
+    dfs(0, adj, vis);
+}
