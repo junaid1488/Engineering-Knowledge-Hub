2ade8f0cc2f341424cb7aacbecc517397f3dfa81
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-27 17:50:36 +0530

    Add Dijkstra's algorithm implementation
    Implemented Dijkstra's algorithm to find shortest paths from the source vertex to all other vertices in a graph.

--- CPP-Interview-Questions /63_Dijkstra.cpp
+++ CPP-Interview-Questions /63_Dijkstra.cpp
@@ -9,3 +9,43 @@ Topic     : Dijkstra's Algorithm
 // Uses a priority queue to select the minimum distance vertex.
 // Relaxes adjacent edges to update shortest distances.
 // Prints the shortest distance from the source to every vertex.
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    int V = 5;
+    vector<vector<pair<int, int>>> adj(V);
+
+    adj[0].push_back({1, 2});
+    adj[0].push_back({2, 4});
+    adj[1].push_back({2, 1});
+    adj[1].push_back({3, 7});
+    adj[2].push_back({4, 3});
+    adj[3].push_back({4, 1});
+
+    vector<int> dist(V, 1e9);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    dist[0] = 0;
+    pq.push({0, 0});
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+
+        if (d > dist[u])
+            continue;
+
+        for (auto [v, w] : adj[u]) {
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    for (int d : dist)
+        cout << d << " ";
+}
