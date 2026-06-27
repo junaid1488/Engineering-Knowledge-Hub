ba4a896fa43a6c22bcf3c6f7ea3a3a88ec4817b2
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-27 17:45:25 +0530

    Enhance Dijkstra.cpp with explanatory comments
    Added comments to explain the Dijkstra's Algorithm implementation.

--- CPP-Interview-Questions /63_Dijkstra.cpp
+++ CPP-Interview-Questions /63_Dijkstra.cpp
@@ -4,3 +4,8 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Dijkstra's Algorithm
 */
+// Represents the graph using an adjacency list.
+// Initializes the distance array with infinity.
+// Uses a priority queue to select the minimum distance vertex.
+// Relaxes adjacent edges to update shortest distances.
+// Prints the shortest distance from the source to every vertex.
