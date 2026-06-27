a6773ddfef798176b2e88e0b164756f79fa60b62
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-27 17:44:46 +0530

    Enhance BFS.cpp with explanatory comments
    Added comments to explain the BFS implementation.

--- CPP-Interview-Questions /61_BFS.cpp
+++ CPP-Interview-Questions /61_BFS.cpp
@@ -4,3 +4,8 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Breadth First Search (BFS)
 */
+// Creates an adjacency list to represent the graph.
+// Uses a queue to perform Breadth First Search.
+// Marks visited vertices to avoid revisiting.
+// Traverses all reachable vertices level by level.
+// Displays the BFS traversal sequence.
