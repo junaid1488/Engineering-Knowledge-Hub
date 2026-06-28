bbb5fd8cba3084627c3cfb794368fa196f683f91
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:41:10 +0530

    Remove comments from Kruskal's algorithm implementation
    Removed comments explaining the functionality of Kruskal's Algorithm.

--- CPP-Interview-Questions /67_Kruskals.cpp
+++ CPP-Interview-Questions /67_Kruskals.cpp
@@ -4,11 +4,7 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Kruskal's Algorithm
 */
-// Stores all graph edges with their weights.
-// Sorts edges in increasing order of weight.
-// Uses Disjoint Set Union (Union-Find) for cycle detection.
-// Selects valid edges for the Minimum Spanning Tree.
-// Displays the total weight of the Minimum Spanning Tree.
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
