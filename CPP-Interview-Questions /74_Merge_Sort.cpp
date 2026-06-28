74796736fab7170c34788a36f370108c8410fb1d
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:30:05 +0530

    Add comments for merge sort algorithm explanation
    Added comments to explain the merge sort algorithm steps.

--- CPP-Interview-Questions /74_Merge_Sort.cpp
+++ CPP-Interview-Questions /74_Merge_Sort.cpp
@@ -4,3 +4,8 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Merge Sort
 */
+// Divide the array into halves.
+// Sort both halves recursively.
+// Merge the sorted halves.
+// Repeat until fully sorted.
+// Display the sorted array.
