9a3272c513acfae56d1c2065e3547d395617c4c4
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:31:44 +0530

    Implement Bubble Sort algorithm in C++

--- CPP-Interview-Questions /71_Bubble_Sort.cpp
+++ CPP-Interview-Questions /71_Bubble_Sort.cpp
@@ -9,3 +9,18 @@ Topic     : Bubble Sort
 // Swap if elements are in the wrong order.
 // Repeat until the array is sorted.
 // Display the sorted array.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[] = {64, 34, 25, 12, 22};
+    int n = 5;
+
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (a[j] > a[j + 1])
+                swap(a[j], a[j + 1]);
+
+    for (int x : a)
+        cout << x << " ";
+}
