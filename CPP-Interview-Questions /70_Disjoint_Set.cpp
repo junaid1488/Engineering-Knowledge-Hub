be3bf49c13014c55be42ee5c9f5cbf87c8e4a673
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:31:28 +0530

    Add Disjoint Set Union implementation in C++
    Implemented Disjoint Set Union (DSU) with path compression and union by rank. Added main function to demonstrate usage.

--- CPP-Interview-Questions /70_Disjoint_Set.cpp
+++ CPP-Interview-Questions /70_Disjoint_Set.cpp
@@ -9,3 +9,48 @@ Topic     : Disjoint Set Union (DSU)
 // Implement Union operation by rank.
 // Merge sets efficiently.
 // Display the result.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class DSU {
+    vector<int> parent, rankv;
+
+public:
+    DSU(int n) {
+        parent.resize(n);
+        rankv.assign(n, 0);
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        return parent[x] == x ? x : parent[x] = find(parent[x]);
+    }
+
+    void unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a == b) return;
+
+        if (rankv[a] < rankv[b])
+            swap(a, b);
+
+        parent[b] = a;
+
+        if (rankv[a] == rankv[b])
+            rankv[a]++;
+    }
+};
+
+int main() {
+    DSU dsu(5);
+
+    dsu.unite(0, 1);
+    dsu.unite(1, 2);
+
+    cout << (dsu.find(0) == dsu.find(2));
+
+    return 0;
+}
