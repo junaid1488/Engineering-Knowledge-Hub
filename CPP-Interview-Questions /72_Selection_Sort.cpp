af47d6976c0edba3fd7e0cc557004466ea084b58
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:32:00 +0530

    Add selection sort implementation in C++
    Implemented selection sort algorithm to sort an array of integers and display the sorted result.

--- CPP-Interview-Questions /72_Selection_Sort.cpp
+++ CPP-Interview-Questions /72_Selection_Sort.cpp
@@ -9,3 +9,23 @@ Topic     : Selection Sort
 // Swap with the current position.
 // Repeat for all positions.
 // Display the sorted array.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[] = {64, 25, 12, 22, 11};
+    int n = 5;
+
+    for (int i = 0; i < n - 1; i++) {
+        int mn = i;
+
+        for (int j = i + 1; j < n; j++)
+            if (a[j] < a[mn])
+                mn = j;
+
+        swap(a[i], a[mn]);
+    }
+
+    for (int x : a)
+        cout << x << " ";
+}
