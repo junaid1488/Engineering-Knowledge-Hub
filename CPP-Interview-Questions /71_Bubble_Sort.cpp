1a8022273ba0a4a45e308f2a714984e2762cbeb8
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:37:37 +0530

    Remove header comments from 71_Bubble_Sort.cpp
    Removed file header comments from Bubble Sort implementation.

--- CPP-Interview-Questions /71_Bubble_Sort.cpp
+++ CPP-Interview-Questions /71_Bubble_Sort.cpp
@@ -1,9 +1,4 @@
-/*
-File Name : 71_Bubble_Sort.cpp
-Author    : Mohd Juned
-Language  : C++
-Topic     : Bubble Sort
-*/
+
 // Input array elements.
 // Compare adjacent elements.
 // Swap if elements are in the wrong order.
