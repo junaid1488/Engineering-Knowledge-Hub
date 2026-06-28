d5c05966ffc3eb1919aed4935ddb117aa895888e
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:29:31 +0530

    Update comments in Selection Sort implementation

--- CPP-Interview-Questions /72_Selection_Sort.cpp
+++ CPP-Interview-Questions /72_Selection_Sort.cpp
@@ -4,3 +4,8 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Selection Sort
 */
+// Input array elements.
+// Find the minimum element.
+// Swap with the current position.
+// Repeat for all positions.
+// Display the sorted array.
