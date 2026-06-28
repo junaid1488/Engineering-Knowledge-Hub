56da78519ae12d4ef7470d3745570f38e2f68f77
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:29:47 +0530

    Add comments to Insertion Sort implementation

--- CPP-Interview-Questions /73_Insertion_Sort.cpp
+++ CPP-Interview-Questions /73_Insertion_Sort.cpp
@@ -4,3 +4,8 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Insertion Sort
 */
+// Input array elements.
+// Pick one element at a time.
+// Insert it into the correct position.
+// Repeat until the array is sorted.
+// Display the sorted array.
