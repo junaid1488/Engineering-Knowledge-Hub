c257f1bb4ae5ebe2bb3b048d09b75a4726e0dfcc
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:38:38 +0530

    Remove comments from Merge Sort implementation
    Removed comments explaining the merge sort algorithm steps.

--- CPP-Interview-Questions /74_Merge_Sort.cpp
+++ CPP-Interview-Questions /74_Merge_Sort.cpp
@@ -4,11 +4,7 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Merge Sort
 */
-// Divide the array into halves.
-// Sort both halves recursively.
-// Merge the sorted halves.
-// Repeat until fully sorted.
-// Display the sorted array.
+
 #include <iostream>
 using namespace std;
 
