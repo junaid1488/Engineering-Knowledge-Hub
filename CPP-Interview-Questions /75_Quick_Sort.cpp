8c58bb4e673c73176475f3f9a1c481c0da49223a
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:33:00 +0530

    Add Quick Sort implementation in C++
    Implemented the Quick Sort algorithm with partitioning and recursive sorting. Added a main function to demonstrate sorting an array.

--- CPP-Interview-Questions /75_Quick_Sort.cpp
+++ CPP-Interview-Questions /75_Quick_Sort.cpp
@@ -9,3 +9,35 @@ Topic     : Quick Sort
 // Recursively sort left subarray.
 // Recursively sort right subarray.
 // Display the sorted array.
+#include <iostream>
+using namespace std;
+
+int partition(int a[], int l, int h) {
+    int pivot = a[h], i = l - 1;
+
+    for (int j = l; j < h; j++)
+        if (a[j] < pivot)
+            swap(a[++i], a[j]);
+
+    swap(a[i + 1], a[h]);
+    return i + 1;
+}
+
+void quickSort(int a[], int l, int h) {
+    if (l < h) {
+        int p = partition(a, l, h);
+
+        quickSort(a, l, p - 1);
+        quickSort(a, p + 1, h);
+    }
+}
+
+int main() {
+    int a[] = {10, 7, 8, 9, 1};
+    int n = 5;
+
+    quickSort(a, 0, n - 1);
+
+    for (int x : a)
+        cout << x << " ";
+}
