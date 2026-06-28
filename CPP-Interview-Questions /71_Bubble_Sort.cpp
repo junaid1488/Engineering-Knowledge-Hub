dccfc734d6bbc9dfe0e09c800d193e2d4681bb26
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-28 12:29:13 +0530

    Add comments to Bubble Sort implementation

--- CPP-Interview-Questions /71_Bubble_Sort.cpp
+++ CPP-Interview-Questions /71_Bubble_Sort.cpp
@@ -4,3 +4,8 @@ Author    : Mohd Juned
 Language  : C++
 Topic     : Bubble Sort
 */
+// Input array elements.
+// Compare adjacent elements.
+// Swap if elements are in the wrong order.
+// Repeat until the array is sorted.
+// Display the sorted array.
