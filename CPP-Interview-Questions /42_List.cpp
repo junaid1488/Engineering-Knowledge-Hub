f43536137a065f73fe43e54153d882476db658c8
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-29 08:49:02 +0530

    Implement basic list operations in C++

--- CPP-Interview-Questions /42_List.cpp
+++ CPP-Interview-Questions /42_List.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <list>
+using namespace std;
 
+int main() {
+    list<int> l = {10, 20, 30};
+
+    l.push_front(5);
+    l.push_back(40);
+
+    cout << "Elements: ";
+    for (int x : l)
+        cout << x << " ";
+
+    l.pop_front();
+    l.pop_back();
+
+    cout << "\nAfter Pop: ";
+    for (int x : l)
+        cout << x << " ";
+
+    cout << "\nFront: " << l.front();
+    cout << "\nBack: " << l.back();
+    cout << "\nSize: " << l.size();
+
+    return 0;
+}
