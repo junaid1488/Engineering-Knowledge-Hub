8a3ab86be74b9342f863dabf210339c0ed2a895d
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-29 08:50:50 +0530

    Implement basic stack operations using STL

--- CPP-Interview-Questions /44_Stack_STL.cpp
+++ CPP-Interview-Questions /44_Stack_STL.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
 
+int main() {
+    stack<int> st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << "Top: " << st.top();
+
+    st.pop();
+
+    cout << "\nAfter Pop, Top: " << st.top();
+    cout << "\nSize: " << st.size();
+
+    cout << "\nElements: ";
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+
+    return 0;
+}
