202cfcd6c428b815a175273b36ac2e99d17feec4
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-29 08:49:54 +0530

    Implement basic deque operations in 43_Deque.cpp
    Demonstrate basic deque operations including push, pop, and accessing front and back elements.

--- CPP-Interview-Questions /43_Deque.cpp
+++ CPP-Interview-Questions /43_Deque.cpp
@@ -1,6 +1,27 @@
-/*
-File Name : 43_Deque.cpp
-Author    : Mohd Juned
-Language  : C++
-Topic     : Deque (STL)
-*/
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+    deque<int> dq = {20, 30, 40};
+
+    dq.push_front(10);
+    dq.push_back(50);
+
+    cout << "Elements: ";
+    for (int x : dq)
+        cout << x << " ";
+
+    dq.pop_front();
+    dq.pop_back();
+
+    cout << "\nAfter Pop: ";
+    for (int x : dq)
+        cout << x << " ";
+
+    cout << "\nFront: " << dq.front();
+    cout << "\nBack: " << dq.back();
+    cout << "\nSize: " << dq.size();
+
+    return 0;
+}
