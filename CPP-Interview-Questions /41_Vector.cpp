cc84f66a0d0c08a86b72cac76a1d7a95ba7c40c4
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-29 08:48:09 +0530

    Implement vector operations in 41_Vector.cpp

--- CPP-Interview-Questions /41_Vector.cpp
+++ CPP-Interview-Questions /41_Vector.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    vector<int> v = {10, 20, 30, 40, 50};
+
+    cout << "Elements: ";
+    for (int x : v)
+        cout << x << " ";
+
+    cout << "\nSize: " << v.size();
+
+    v.push_back(60);
+    cout << "\nAfter Push: ";
+    for (int x : v)
+        cout << x << " ";
+
+    v.pop_back();
+    cout << "\nAfter Pop: ";
+    for (int x : v)
+        cout << x << " ";
+
+    cout << "\nFront: " << v.front();
+    cout << "\nBack: " << v.back();
+
+    return 0;
+}
