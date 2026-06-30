1364181b1e2c2d6fb879125e2b1078ac417305c0
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-30 14:32:36 +0530

    Add comments to organize Binary Search code
    Added comments for code structure and sections.

--- CPP-Interview-Questions /76_Binary_Search.cpp
+++ CPP-Interview-Questions /76_Binary_Search.cpp
@@ -1,3 +1,14 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Binary Search
+
+
+// Header Files
+// Namespace Declaration
+// Binary Search Function
+// Main Function
+// Input Array Initialization
+// Search Key Input
+// Function Call
+// Display Result
+// Program Termination
