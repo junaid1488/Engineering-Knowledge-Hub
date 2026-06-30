dc6f7c3ed2512c962437c5ddc8bc4354bedad754
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-30 14:19:18 +0530

    Clean up comments in 61_BFS.cpp
    Removed header comments and retained function descriptions.

--- CPP-Interview-Questions /61_BFS.cpp
+++ CPP-Interview-Questions /61_BFS.cpp
@@ -1,9 +1,4 @@
-/*
-File Name : 61_BFS.cpp
-Author    : Mohd Juned
-Language  : C++
-Topic     : Breadth First Search (BFS)
-*/
+
 // Creates an adjacency list to represent the graph.
 // Uses a queue to perform Breadth First Search.
 // Marks visited vertices to avoid revisiting.
