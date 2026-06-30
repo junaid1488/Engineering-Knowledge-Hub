76147b4eed4bc44597810917dfa7a509d1946987
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-30 14:19:49 +0530

    Clean up comments in 62_DFS.cpp
    Removed header comments and retained function descriptions.

--- CPP-Interview-Questions /62_DFS.cpp
+++ CPP-Interview-Questions /62_DFS.cpp
@@ -1,9 +1,4 @@
-/*
-File Name : 62_DFS.cpp
-Author    : Mohd Juned
-Language  : C++
-Topic     : Depth First Search (DFS)
-*/
+
 // Creates an adjacency list to represent the graph.
 // Uses recursion to perform Depth First Search.
 // Marks visited vertices to prevent repeated traversal.
