a6a993deeec32eb8b78534d0599fc86a8d8e2d62
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-30 14:23:30 +0530

    Clean up comments in 63_Dijkstra.cpp
    Removed header comments and retained algorithm description.

--- CPP-Interview-Questions /63_Dijkstra.cpp
+++ CPP-Interview-Questions /63_Dijkstra.cpp
@@ -1,9 +1,4 @@
-/*
-File Name : 63_Dijkstra.cpp
-Author    : Mohd Juned
-Language  : C++
-Topic     : Dijkstra's Algorithm
-*/
+
 // Represents the graph using an adjacency list.
 // Initializes the distance array with infinity.
 // Uses a priority queue to select the minimum distance vertex.
