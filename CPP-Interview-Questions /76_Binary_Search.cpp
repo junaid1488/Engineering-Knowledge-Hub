af5a1d0c7fbaeaf2d6980e0c4dc5fec5b3d137e4
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-30 14:34:09 +0530

    Clean up comments in Binary_Search.cpp
    Removed unnecessary comments and blank lines.

--- CPP-Interview-Questions /76_Binary_Search.cpp
+++ CPP-Interview-Questions /76_Binary_Search.cpp
@@ -1,17 +1,6 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Binary Search
-
-
-// Header Files
-// Namespace Declaration
-// Binary Search Function
-// Main Function
-// Input Array Initialization
-// Search Key Input
-// Function Call
-// Display Result
-// Program Termination
 #include <iostream>
 using namespace std;
 
