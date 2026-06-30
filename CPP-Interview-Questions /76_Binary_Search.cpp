381986c4be318a5431cd37bf955f8236b8ec0189
Mohd Juned <akhtaralam9936@gmail.com>
2026-06-30 14:35:36 +0530

    Refactor binary search implementation

--- CPP-Interview-Questions /76_Binary_Search.cpp
+++ CPP-Interview-Questions /76_Binary_Search.cpp
@@ -14,10 +14,8 @@ int binarySearch(int arr[], int n, int key) {
         else
             high = mid - 1;
     }
-
     return -1;
 }
-
 int main() {
     int arr[] = {2, 4, 6, 8, 10, 12, 14, 16};
     int n = sizeof(arr) / sizeof(arr[0]);
@@ -32,6 +30,5 @@ int main() {
         cout << "Element found at index " << index;
     else
         cout << "Element not found";
-
     return 0;
 }
