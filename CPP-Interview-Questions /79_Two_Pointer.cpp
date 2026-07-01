7a68182389f5e9d274023ea9ffe18d84157c7cc9
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 14:12:27 +0530

    Add two-pointer approach for pair sum search
    Implemented two-pointer technique to find a pair that sums to a target value.

--- CPP-Interview-Questions /79_Two_Pointer.cpp
+++ CPP-Interview-Questions /79_Two_Pointer.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int lowerBound(int arr[], int n, int key) {
-    int low = 0, high = n;
-    while (low < high) {
-        int mid = low + (high - low) / 2;
-        if (arr[mid] < key)
-            low = mid + 1;
-        else
-            high = mid;
-    }
-    return low;
-}
-
 int main() {
-    int arr[] = {2, 4, 4, 6, 8, 10};
-    int n = sizeof(arr) / sizeof(arr[0]), key;
-    cout << "Enter element: ";
-    cin >> key;
-    cout << "Lower Bound Index: " << lowerBound(arr, n, key);
+    int arr[] = {1, 2, 3, 4, 6, 8, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+
+    int left = 0, right = n - 1;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == target) {
+            cout << "Pair Found: " << arr[left] << " " << arr[right];
+            return 0;
+        }
+        if (sum < target)
+            left++;
+        else
+            right--;
+    }
+
+    cout << "Pair Not Found";
     return 0;
 }
