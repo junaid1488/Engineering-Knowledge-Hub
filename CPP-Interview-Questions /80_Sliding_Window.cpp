c813c5a410737cf9f803f66942d093d07242f7b4
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 14:13:15 +0530

    Add sliding window algorithm for maximum sum
    Implement sliding window algorithm to find maximum sum of subarray.

--- CPP-Interview-Questions /80_Sliding_Window.cpp
+++ CPP-Interview-Questions /80_Sliding_Window.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int arr[] = {2, 1, 5, 1, 3, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 3;
+
+    int sum = 0;
+    for (int i = 0; i < k; i++)
+        sum += arr[i];
+
+    int maxSum = sum;
+
+    for (int i = k; i < n; i++) {
+        sum += arr[i] - arr[i - k];
+        if (sum > maxSum)
+            maxSum = sum;
+    }
+
+    cout << "Maximum Sum: " << maxSum;
+    return 0;
+}
