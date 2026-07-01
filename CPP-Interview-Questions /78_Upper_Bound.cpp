eb5859503b78948ffd447a3142070d67b0c51ce2
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 14:01:50 +0530

    Enhance comments in 78_Upper_Bound.cpp
    Added comments for better code organization and clarity.

--- CPP-Interview-Questions /78_Upper_Bound.cpp
+++ CPP-Interview-Questions /78_Upper_Bound.cpp
@@ -1,3 +1,16 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Upper Bound
+// Author: Mohd Juned
+// Language: C++
+// Topic: Upper Bound
+
+// Header Files
+// Namespace Declaration
+// Upper Bound Function
+// Main Function
+// Input Array Initialization
+// Search Key Input
+// Function Call
+// Display Result
+// Program Termination
