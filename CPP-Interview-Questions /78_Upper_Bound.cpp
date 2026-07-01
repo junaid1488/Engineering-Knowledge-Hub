3be8ae660d668c2edcba4724db32245580999b2b
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 14:02:03 +0530

    Clean up comments in Upper Bound implementation
    Removed unnecessary comments from the code.

--- CPP-Interview-Questions /78_Upper_Bound.cpp
+++ CPP-Interview-Questions /78_Upper_Bound.cpp
@@ -5,12 +5,4 @@
 // Language: C++
 // Topic: Upper Bound
 
-// Header Files
-// Namespace Declaration
-// Upper Bound Function
-// Main Function
-// Input Array Initialization
-// Search Key Input
-// Function Call
-// Display Result
-// Program Termination
+
