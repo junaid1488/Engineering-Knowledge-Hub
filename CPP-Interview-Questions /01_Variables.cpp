ba0859e9675d01dbdd564b5848bd8931b8fa529f
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 13:57:54 +0530

    Fix formatting issues in 01_Variables.cpp

--- CPP-Interview-Questions /01_Variables.cpp
+++ CPP-Interview-Questions /01_Variables.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int age = 21;
     double cgpa = 8.5;
     char grade = 'A';
     bool placed = false;
     string name = "Juned";
-
     cout << "Name: " << name << '\n';
     cout << "Age: " << age << '\n';
     cout << "CGPA: " << cgpa << '\n';
     cout << "Grade: " << grade << '\n';
     cout << "Placed: " << placed << '\n';
-
     return 0;
 }
