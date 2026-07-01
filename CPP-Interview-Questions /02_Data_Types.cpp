f598954bc36c9071c06ee98bb00e757ebe6afe21
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 13:58:33 +0530

    Remove unnecessary whitespace in Data_Types.cpp

--- CPP-Interview-Questions /02_Data_Types.cpp
+++ CPP-Interview-Questions /02_Data_Types.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int a = 10;
     long long b = 10000000000LL;
@@ -8,13 +7,11 @@ int main() {
     double d = 20.123456;
     char e = 'J';
     bool f = true;
-
     cout << sizeof(a) << '\n';
     cout << sizeof(b) << '\n';
     cout << sizeof(c) << '\n';
     cout << sizeof(d) << '\n';
     cout << sizeof(e) << '\n';
     cout << sizeof(f) << '\n';
-
     return 0;
 }
