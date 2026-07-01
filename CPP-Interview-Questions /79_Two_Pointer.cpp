56035afbce73116304c57c35c3733a2403bb516a
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 14:02:50 +0530

    Update 79_Two_Pointer.cpp

--- CPP-Interview-Questions /79_Two_Pointer.cpp
+++ CPP-Interview-Questions /79_Two_Pointer.cpp
@@ -1,3 +1,15 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Two Pointer Technique
+// Author: Mohd Juned
+// Language: C++
+// Topic: Two Pointer Technique
+
+// Header Files
+// Namespace Declaration
+// Two Pointer Function
+// Main Function
+// Input Array Initialization
+// Function Call
+// Display Result
+// Program Termination
