52637d8da8654d568371242ad7ad6b367d72d515
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 14:12:07 +0530

    Add lowerBound function for sorted array
    Implemented a lower bound function to find the index of the first element not less than a given key in a sorted array.

--- CPP-Interview-Questions /79_Two_Pointer.cpp
+++ CPP-Interview-Questions /79_Two_Pointer.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+using namespace std;
 
-//klhsl;sahd
-///jksvlksdhfhds
+int lowerBound(int arr[], int n, int key) {
+    int low = 0, high = n;
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid;
+    }
+    return low;
+}
+
+int main() {
+    int arr[] = {2, 4, 4, 6, 8, 10};
+    int n = sizeof(arr) / sizeof(arr[0]), key;
+    cout << "Enter element: ";
+    cin >> key;
+    cout << "Lower Bound Index: " << lowerBound(arr, n, key);
+    return 0;
+}
