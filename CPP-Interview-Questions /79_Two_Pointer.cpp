9244ed2c3699e42ca7d5495fcbbf2687e0f681f3
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 14:03:01 +0530

    Clean up comments in 79_Two_Pointer.cpp
    Removed unnecessary comments from the code.

--- CPP-Interview-Questions /79_Two_Pointer.cpp
+++ CPP-Interview-Questions /79_Two_Pointer.cpp
@@ -3,13 +3,3 @@
 // Topic: Two Pointer Technique
 // Author: Mohd Juned
 // Language: C++
-// Topic: Two Pointer Technique
-
-// Header Files
-// Namespace Declaration
-// Two Pointer Function
-// Main Function
-// Input Array Initialization
-// Function Call
-// Display Result
-// Program Termination
