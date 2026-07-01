af5610bde73d30761b140a43faa220a7cfb54360
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 14:00:43 +0530

    Enhance comments in 77_Lower_Bound.cpp
    Added comments for better code understanding.

--- CPP-Interview-Questions /77_Lower_Bound.cpp
+++ CPP-Interview-Questions /77_Lower_Bound.cpp
@@ -1,3 +1,16 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Lower Bound
+// Author: Mohd Juned
+// Language: C++
+// Topic: Lower Bound
+
+// Header Files
+// Namespace Declaration
+// Lower Bound Function
+// Main Function
+// Input Array Initialization
+// Search Key Input
+// Function Call
+// Display Result
+// Program Termination
