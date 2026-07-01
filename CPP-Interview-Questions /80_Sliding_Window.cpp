ad034a791bcc80a66b2e3f8cf350b3797f097088
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-01 14:04:17 +0530

    Enhance code documentation with comments
    Added comments for better code documentation.

--- CPP-Interview-Questions /80_Sliding_Window.cpp
+++ CPP-Interview-Questions /80_Sliding_Window.cpp
@@ -1,3 +1,16 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Sliding Window
+// Author: Mohd Juned
+// Language: C++
+// Topic: Sliding Window
+
+// Header Files
+// Namespace Declaration
+// Sliding Window Function
+// Main Function
+// Input Array Initialization
+// Window Size Input
+// Function Call
+// Display Result
+// Program Termination
