8fc37aa244e36dd83b8af416917c09d38970512b
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-02 14:27:23 +0530

    Refactor LCS function into main and simplify code

--- CPP-Interview-Questions /83_LCS.cpp
+++ CPP-Interview-Questions /83_LCS.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
-int lcs(string a, string b) {
+int main() {
+    string a = "ABCBDAB", b = "BDCAB";
     int n = a.size(), m = b.size();
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            if (a[i - 1] == b[j - 1])
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            else
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-        }
-    }
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            dp[i][j] = (a[i - 1] == b[j - 1])
+                       ? dp[i - 1][j - 1] + 1
+                       : max(dp[i - 1][j], dp[i][j - 1]);
 
-    return dp[n][m];
-}
-
-int main() {
-    string s1 = "ABCBDAB";
-    string s2 = "BDCAB";
-
-    cout << "Length of LCS: " << lcs(s1, s2);
-
+    cout << "Length of LCS: " << dp[n][m];
     return 0;
 }
