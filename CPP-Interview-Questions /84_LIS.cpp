0e1a8b835f33881adc246a82b089688777e2eb82
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-03 20:05:22 +0530

    Add Longest Increasing Subsequence implementation
    Implement Longest Increasing Subsequence algorithm using dynamic programming.

--- CPP-Interview-Questions /84_LIS.cpp
+++ CPP-Interview-Questions /84_LIS.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    vector<int> a = {10, 9, 2, 5, 3, 7, 101, 18};
+    int n = a.size(), ans = 1;
+    vector<int> dp(n, 1);
+
+    for (int i = 1; i < n; i++)
+        for (int j = 0; j < i; j++)
+            if (a[i] > a[j])
+                dp[i] = max(dp[i], dp[j] + 1);
+
+    for (int x : dp) ans = max(ans, x);
+
+    cout << "Length of LIS: " << ans;
+    return 0;
+}
