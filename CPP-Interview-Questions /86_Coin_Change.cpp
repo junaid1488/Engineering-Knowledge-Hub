c1d9360a9b03693d70e27c004fe6e768d97b5d1f
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-04 16:27:22 +0530

    Add coin change solution using dynamic programming
    Implement dynamic programming solution for coin change problem.

--- CPP-Interview-Questions /86_Coin_Change.cpp
+++ CPP-Interview-Questions /86_Coin_Change.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
 
+int main() {
+    vector<int> coins = {1, 2, 5};
+    int amount = 11;
+
+    vector<int> dp(amount + 1, INT_MAX);
+    dp[0] = 0;
+
+    for (int coin : coins)
+        for (int i = coin; i <= amount; i++)
+            if (dp[i - coin] != INT_MAX)
+                dp[i] = min(dp[i], dp[i - coin] + 1);
+
+    cout << (dp[amount] == INT_MAX ? -1 : dp[amount]);
+    return 0;
+}
