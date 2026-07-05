2d69aade6430ab82f97424599b9bcc89339ca75e
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-05 19:55:43 +0530

    Clean up comments in 88_Subset_Sum.cpp
    Removed commented-out code and author information.

--- CPP-Interview-Questions /88_Subset_Sum.cpp
+++ CPP-Interview-Questions /88_Subset_Sum.cpp
@@ -1,16 +1,1 @@
-  // Author: Mohd Juned
-// Language: C++
-// Topic: Rod Cutting using Dynamic Programming
 
-// Include Header Files
-// Declare Namespace
-
-// Rod Cutting Function
-
-// Main Function
-// Initialize Price Array
-// Create DP Array
-// Compute Maximum Profit
-// Print Final Result
-
-// End of Program
