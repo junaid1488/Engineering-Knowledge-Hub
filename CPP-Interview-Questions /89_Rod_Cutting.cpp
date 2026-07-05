8b7ff83c70a740c1ea5f03189d449cd0970626b5
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-05 19:56:24 +0530

    Enhance Rod Cutting code with comments and structure
    Added comments and structure to the Rod Cutting program.

--- CPP-Interview-Questions /89_Rod_Cutting.cpp
+++ CPP-Interview-Questions /89_Rod_Cutting.cpp
@@ -1,1 +1,16 @@
+// Author: Mohd Juned
+// Language: C++
+// Topic: Rod Cutting using Dynamic Programming
 
+// Include Header Files
+// Declare Namespace
+
+// Rod Cutting Function
+
+// Main Function
+// Initialize Price Array
+// Create DP Array
+// Compute Maximum Profit
+// Print Final Result
+
+// End of Program
