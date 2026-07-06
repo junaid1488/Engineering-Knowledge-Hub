84f986a94cc0e3061e234702aadf20c1011f9f23
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-06 20:31:28 +0530

    Add basic threading example in 91_Thread_Basics.cpp

--- CPP-Interview-Questions /91_Thread_Basics.cpp
+++ CPP-Interview-Questions /91_Thread_Basics.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <thread>
+using namespace std;
 
+void printMessage() {
+    cout << "Thread is running..." << endl;
+}
+
+int main() {
+    thread t(printMessage);
+
+    t.join();
+
+    cout << "Main thread finished." << endl;
+
+    return 0;
+}
