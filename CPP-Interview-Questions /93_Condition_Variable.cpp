07cdc98d33e27db739413b4d8c820e8bd73895d0
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-07 22:20:12 +0530

    Clean up comments in 93_Condition_Variable.cpp
    Removed comments outlining the structure of the program.

--- CPP-Interview-Questions /93_Condition_Variable.cpp
+++ CPP-Interview-Questions /93_Condition_Variable.cpp
@@ -1,17 +1,3 @@
 // Author: Mohd Juned
 // Language: C++
 // Topic: Condition Variable in Multithreading
-
-// Include Required Header Files
-// Use Standard Namespace
-
-// Shared Variables Declaration
-// Producer/Consumer Function
-// Condition Variable Notification
-
-// Main Function
-// Create Thread Objects
-// Wait and Notify Threads
-// Display Final Output
-
-// End of Program
