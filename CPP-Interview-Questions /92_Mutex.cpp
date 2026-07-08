ca7a36034a268daae019255f7aec5770157eca22
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-08 22:14:09 +0530

    Implement mutex for thread-safe counter increment
    Added a mutex to synchronize access to a counter variable in a multi-threaded environment.

--- CPP-Interview-Questions /92_Mutex.cpp
+++ CPP-Interview-Questions /92_Mutex.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
 
+mutex mtx;
+int counter = 0;
+
+void increment() {
+    for (int i = 0; i < 1000; i++) {
+        mtx.lock();
+        counter++;
+        mtx.unlock();
+    }
+}
+
+int main() {
+    thread t1(increment), t2(increment);
+
+    t1.join();
+    t2.join();
+
+    cout << "Counter: " << counter;
+
+    return 0;
+}
