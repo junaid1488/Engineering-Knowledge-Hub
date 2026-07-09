11820c43015af2f32b50acf0b71416e85cdc850b
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-09 20:33:52 +0530

    Clean up comments in 94_Producer_Consumer.cpp
    Removed author and topic comments from the code.

--- CPP-Interview-Questions /94_Producer_Consumer.cpp
+++ CPP-Interview-Questions /94_Producer_Consumer.cpp
@@ -9,17 +9,6 @@
 // Producer Function
 // Consumer Function
 
-// Author: Mohd Juned
-// Language: C++
-// Topic: Producer Consumer Problem
-
-// Include Required Header Files
-// Use Standard Namespace
-
-// Shared Resource Declaration
-// Producer Function
-// Consumer Function
-
 // Main Function
 // Create Producer and Consumer Threads
 // Synchronize Thread Execution
