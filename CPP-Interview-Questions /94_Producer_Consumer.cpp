a56d6701eed06f89c47e7bd493798bd40163ece8
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-09 20:34:32 +0530

    Remove redundant comments from Producer-Consumer code

--- CPP-Interview-Questions /94_Producer_Consumer.cpp
+++ CPP-Interview-Questions /94_Producer_Consumer.cpp
@@ -15,20 +15,3 @@
 // Display Final Output
 
 // End of Program
-// Author: Mohd Juned
-// Language: C++
-// Topic: Producer Consumer Problem
-
-// Include Required Header Files
-// Use Standard Namespace
-
-// Shared Resource Declaration
-// Producer Function
-// Consumer Function
-
-// Main Function
-// Create Producer and Consumer Threads
-// Synchronize Thread Execution
-// Display Final Output
-
-// End of Program
