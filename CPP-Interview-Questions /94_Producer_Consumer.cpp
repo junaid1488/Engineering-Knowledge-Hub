a9cef0dc98abf2d9b41032359ac45613973f87f8
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-10 19:55:13 +0530

    Clean up Producer Consumer code structure
    Removed all comments and structure from the Producer Consumer implementation.

--- CPP-Interview-Questions /94_Producer_Consumer.cpp
+++ CPP-Interview-Questions /94_Producer_Consumer.cpp
@@ -1,18 +1,1 @@
 
-// Author: Mohd Juned
-// Language: C++
-// Topic: Producer Consumer Problem
-
-// Include Required Header Files
-// Use Standard Namespace
-
-// Shared Resource Declaration
-// Producer Function
-// Consumer Function
-
-// Main Function
-// Create Producer and Consumer Threads
-// Synchronize Thread Execution
-// Display Final Output
-
-// End of Program
