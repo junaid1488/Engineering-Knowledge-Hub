7cde7e064e8a5faf9938425add0dfa8047c91ab3
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-12 19:54:32 +0530

    Enhance comments in 94_Producer_Consumer.cpp
    Added comments for clarity and author information.

--- CPP-Interview-Questions /94_Producer_Consumer.cpp
+++ CPP-Interview-Questions /94_Producer_Consumer.cpp
@@ -13,6 +13,22 @@
 // Main Function
 // Create Producer and Consumer Threads
 // Synchronize Thread Execution
+// Display Final Output// Author: Mohd Juned
+// Language: C++
+// Topic: Producer Consumer Problem
+
+// Include Required Header Files
+// Use Standard Namespace
+
+// Shared Resource Declaration
+// Producer Function
+// Consumer Function
+
+// Main Function
+// Create Producer and Consumer Threads
+// Synchronize Thread Execution
 // Display Final Output
+
+// End of Program
 
 // End of Program
