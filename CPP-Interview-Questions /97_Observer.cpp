916e27c3d7b8bb5cbfc523b97d8a9cb1aec028ba
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-16 11:46:41 +0530

    Add Observer pattern implementation in C++
    Implemented Observer design pattern with User and Subject classes.

--- CPP-Interview-Questions /97_Observer.cpp
+++ CPP-Interview-Questions /97_Observer.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+class Observer {
+public:
+    virtual void update() = 0;
+};
+
+class User : public Observer {
+public:
+    void update() {
+        cout << "Notification Received\n";
+    }
+};
+
+class Subject {
+    vector<Observer*> obs;
+
+public:
+    void attach(Observer* o) {
+        obs.push_back(o);
+    }
+
+    void notify() {
+        for (auto o : obs)
+            o->update();
+    }
+};
+
+int main() {
+    Subject s;
+    User u1, u2;
+
+    s.attach(&u1);
+    s.attach(&u2);
+
+    s.notify();
+
+    return 0;
+}
