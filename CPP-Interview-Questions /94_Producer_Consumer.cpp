ae9bf78249270137497a2199bda23a1b9f4c0e7e
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-16 11:45:32 +0530

    Add producer-consumer implementation in C++
    Implement producer-consumer problem using threads, mutex, and condition variables.

--- CPP-Interview-Questions /94_Producer_Consumer.cpp
+++ CPP-Interview-Questions /94_Producer_Consumer.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+using namespace std;
 
+queue<int> q;
+mutex mtx;
+condition_variable cv;
+
+void producer() {
+    for (int i = 1; i <= 5; i++) {
+        unique_lock<mutex> lock(mtx);
+        q.push(i);
+        cout << "Produced: " << i << endl;
+        lock.unlock();
+        cv.notify_one();
+    }
+}
+
+void consumer() {
+    for (int i = 1; i <= 5; i++) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [] { return !q.empty(); });
+        cout << "Consumed: " << q.front() << endl;
+        q.pop();
+    }
+}
+
+int main() {
+    thread p(producer), c(consumer);
+    p.join();
+    c.join();
+    return 0;
+}
