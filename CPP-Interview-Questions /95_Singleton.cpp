771ca45d43c9bdbc17752a65b25f05ad51fb290a
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-16 11:45:52 +0530

    Implement Singleton pattern in C++

--- CPP-Interview-Questions /95_Singleton.cpp
+++ CPP-Interview-Questions /95_Singleton.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+class Singleton {
+private:
+    static Singleton* instance;
+    Singleton() {}
+
+public:
+    static Singleton* getInstance() {
+        if (!instance)
+            instance = new Singleton();
+        return instance;
+    }
+
+    void show() {
+        cout << "Singleton Object Created";
+    }
+};
+
+Singleton* Singleton::instance = nullptr;
+
+int main() {
+    Singleton::getInstance()->show();
+    return 0;
+}
