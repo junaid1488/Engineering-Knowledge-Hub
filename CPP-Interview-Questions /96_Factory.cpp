25adf34e60426e59cd19185f9a35294fd74cc39e
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-16 11:46:19 +0530

    Add Factory pattern implementation for Shape classes
    Implemented a Factory design pattern to create Shape objects.

--- CPP-Interview-Questions /96_Factory.cpp
+++ CPP-Interview-Questions /96_Factory.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+class Shape {
+public:
+    virtual void draw() = 0;
+};
+
+class Circle : public Shape {
+public:
+    void draw() { cout << "Circle\n"; }
+};
+
+class Square : public Shape {
+public:
+    void draw() { cout << "Square\n"; }
+};
+
+class Factory {
+public:
+    static Shape* create(char ch) {
+        if (ch == 'C') return new Circle();
+        return new Square();
+    }
+};
+
+int main() {
+    Shape* obj = Factory::create('C');
+    obj->draw();
+    delete obj;
+    return 0;
+}
