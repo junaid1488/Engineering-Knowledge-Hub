1b365404a9d8276e8f44279b7f2616799f03f1b8
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-17 19:52:09 +0530

    Remove commented lines from 98_Smart_Pointers.cpp

--- CPP-Interview-Questions /98_Smart_Pointers.cpp
+++ CPP-Interview-Questions /98_Smart_Pointers.cpp
@@ -9,12 +9,3 @@
 // Create Unique Pointer
 // Create Shared Pointer
 // Create Weak Pointer
-
-// Main Function
-// Access Managed Objects
-// Demonstrate Smart Pointer Usage
-// Display Output
-// Author: Mohd Juned
-// Language: C++
-// Topic: Smart Pointers
-
