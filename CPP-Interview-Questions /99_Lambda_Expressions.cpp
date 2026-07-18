9bbefedb1bfa3f93e2153736a910ba38d9060a88
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-18 20:08:31 +0530

    Implement lambda expressions for sorting and addition
    This program demonstrates the use of lambda expressions for sorting a vector and for capturing variables in C++. It sorts a list of integers and calculates the sum of two numbers using a lambda function.

--- CPP-Interview-Questions /99_Lambda_Expressions.cpp
+++ CPP-Interview-Questions /99_Lambda_Expressions.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main() {
+    vector<int> nums = {5, 2, 8, 1, 3};
+
+    sort(nums.begin(), nums.end(), [](int a, int b) {
+        return a < b;
+    });
+
+    cout << "Sorted Elements: ";
+
+    for_each(nums.begin(), nums.end(), [](int x) {
+        cout << x << " ";
+    });
+
+    cout << endl;
+
+    int a = 10, b = 20;
+
+    auto add = [=]() {
+        return a + b;
+    };
+
+    cout << "Sum: " << add();
+
+    return 0;
+}
