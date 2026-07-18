1662a74f68baf9cec47fd6e195d8c0689cd8cf78
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-18 20:02:58 +0530

    Clean up comments in Lambda Expressions file
    Removed commented sections for clarity.

--- CPP-Interview-Questions /99_Lambda_Expressions.cpp
+++ CPP-Interview-Questions /99_Lambda_Expressions.cpp
@@ -3,16 +3,4 @@
 // Language: C++
 // Topic: Lambda Expressions
 
-// Include Required Header Files
-// Use Standard Namespace
 
-// Lambda Function Declaration
-// Capture Variables
-// Lambda with Parameters
-// Lambda with Return Value
-
-// Main Function
-// Invoke Lambda Expressions
-// Display Output
-
-// End of Program
