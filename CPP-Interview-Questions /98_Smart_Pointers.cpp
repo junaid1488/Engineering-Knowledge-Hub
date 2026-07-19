0d179917bee93010c8240810bbade78e958b4058
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-19 19:18:46 +0530

    Add comments and author information to smart pointers file

--- CPP-Interview-Questions /98_Smart_Pointers.cpp
+++ CPP-Interview-Questions /98_Smart_Pointers.cpp
@@ -18,3 +18,22 @@
 
 // End of Program
 
+
+// Author: Mohd Juned
+// Language: C++
+// Topic: Smart Pointers
+
+// Include Required Header Files
+// Use Standard Namespace
+
+// Smart Pointer Declaration
+// Create Unique Pointer
+// Create Shared Pointer
+// Create Weak Pointer
+
+// Main Function
+// Access Managed Objects
+// Demonstrate Smart Pointer Usage
+// Display Output
+
+// End of Program
