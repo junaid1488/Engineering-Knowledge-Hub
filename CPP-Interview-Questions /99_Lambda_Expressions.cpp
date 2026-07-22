6cb8a1c262a1206b95ccf88d807c0f7c0f69f4c6
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-22 21:16:16 +0530

    Clean up Lambda Expressions example
    Removed unnecessary code and adjusted formatting.

--- CPP-Interview-Questions /99_Lambda_Expressions.cpp
+++ CPP-Interview-Questions /99_Lambda_Expressions.cpp
@@ -3,17 +3,7 @@
 using namespace std;
 
 class Demo {
-public:
-    Demo() {
-
-
-int main() {
-
-
-
-    weak_ptr<Demo> ptr4 = ptr2;
-
-    if (auto temp = ptr4.lock())
+publictr4.lock())
         temp->show();
 
     return 0;
