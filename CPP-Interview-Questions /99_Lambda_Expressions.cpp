ee3c1720d97e3e48da98cf66d098afffb72b34ef
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-22 20:34:28 +0530

    Enhance comments in 99_Lambda_Expressions.cpp
    Added comments for structure and clarity in the Lambda Expressions example.

--- CPP-Interview-Questions /99_Lambda_Expressions.cpp
+++ CPP-Interview-Questions /99_Lambda_Expressions.cpp
@@ -1,1 +1,17 @@
+// Author: Mohd Juned
+// Language: C++
+// Topic: Lambda Expressions
 
+// Include Required Header Files
+// Use Standard Namespace
+
+// Lambda Function Declaration
+// Capture Variables
+// Lambda with Parameters
+// Lambda with Return Value
+
+// Main Function
+// Invoke Lambda Expressions
+// Display Output
+
+// End of Program
