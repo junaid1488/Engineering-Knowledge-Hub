c4ad78e2e29f833a44458479efe97abe30434129
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-22 21:17:00 +0530

    Implement smart pointers with Demo class

--- CPP-Interview-Questions /99_Lambda_Expressions.cpp
+++ CPP-Interview-Questions /99_Lambda_Expressions.cpp
@@ -1,1 +1,36 @@
 
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class Demo {
+public:
+    Demo() {
+        cout << "Object Created\n";
+    }
+
+    ~Demo() {
+        cout << "Object Destroyed\n";
+    }
+
+    void show() {
+        cout << "Using Smart Pointer\n";
+    }
+};
+
+int main() {
+    unique_ptr<Demo> ptr1 = make_unique<Demo>();
+    ptr1->show();
+
+    shared_ptr<Demo> ptr2 = make_shared<Demo>();
+    shared_ptr<Demo> ptr3 = ptr2;
+
+    cout << "Shared Count: " << ptr2.use_count() << endl;
+
+    weak_ptr<Demo> ptr4 = ptr2;
+
+    if (auto temp = ptr4.lock())
+        temp->show();
+
+    return 0;
+}
