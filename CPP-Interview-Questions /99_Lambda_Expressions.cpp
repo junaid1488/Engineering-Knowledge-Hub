693a2213770e1099dc0891f0088780e3bdd8c786
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-22 21:15:48 +0530

    Refactor main function by removing smart pointer examples
    Removed unique_ptr and shared_ptr examples from main function.

--- CPP-Interview-Questions /99_Lambda_Expressions.cpp
+++ CPP-Interview-Questions /99_Lambda_Expressions.cpp
@@ -8,11 +8,6 @@ public:
 
 
 int main() {
-    unique_ptr<Demo> ptr1 = make_unique<Demo>();
-    ptr1->show();
-
-    shared_ptr<Demo> ptr2 = make_shared<Demo>();
-    shared_ptr<Demo> ptr3 = ptr2;
 
 
 
