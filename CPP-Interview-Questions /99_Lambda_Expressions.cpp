d06d017d650bba4db6c020c9632861a243ebf339
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-22 21:19:03 +0530

    Clean up pointer usage in Lambda Expressions example
    Removed unused shared and weak pointer examples.

--- CPP-Interview-Questions /99_Lambda_Expressions.cpp
+++ CPP-Interview-Questions /99_Lambda_Expressions.cpp
@@ -20,14 +20,7 @@ public:
 
 int main() {
     unique_ptr<Demo> ptr1 = make_unique<Demo>();
-    ptr1->show();
-
-    shared_ptr<Demo> ptr2 = make_shared<Demo>();
-    shared_ptr<Demo> ptr3 = ptr2;
-
-    cout << "Shared Count: " << ptr2.use_count() << endl;
-
-    weak_ptr<Demo> ptr4 = ptr2;
+  
 
     if (auto temp = ptr4.lock())
         temp->show();
