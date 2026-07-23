1e89670588a012537a66a3258d6f7b1159846350
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-23 20:55:02 +0530

    Refactor smart pointer usage in main function
    Removed unique_ptr and shared_ptr examples from main function.

--- CPP-Interview-Questions /98_Smart_Pointers.cpp
+++ CPP-Interview-Questions /98_Smart_Pointers.cpp
@@ -18,11 +18,7 @@ public:
 };
 
 int main() {
-    unique_ptr<Demo> ptr1 = make_unique<Demo>();
-    ptr1->show();
-
-    shared_ptr<Demo> ptr2 = make_shared<Demo>();
-    shared_ptr<Demo> ptr3 = ptr2;
+ 
 
     cout << "Shared Count: " << ptr2.use_count() << endl;
 
