e3bb7f7fbdec8bf7542c0bb88f8bd404f2bca7c7
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-27 14:27:59 +0530

    Remove sorting of nums vector with lambda
    Removed the sorting of the nums vector using a lambda expression.

--- CPP-Interview-Questions /99_Lambda_Expressions.cpp
+++ CPP-Interview-Questions /99_Lambda_Expressions.cpp
@@ -4,11 +4,7 @@
 using namespace std;
 
 int main() {
-    vector<int> nums = {5, 2, 8, 1, 3};
 
-    sort(nums.begin(), nums.end(), [](int a, int b) {
-        return a < b;
-    });
 
 
     cout << endl;
