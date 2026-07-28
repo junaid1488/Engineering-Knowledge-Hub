705e0aa2cf4153cecdee0c27cbe99c837b6653fc
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-28 12:39:27 +0530

    Add example of smart pointers in C++
    Demonstrate usage of unique_ptr, shared_ptr, and weak_ptr in C++.

--- CPP-Interview-Questions /98_Smart_Pointers.cpp
+++ CPP-Interview-Questions /98_Smart_Pointers.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <memory>
+using namespace std;
 
+class Student {
+public:
+    Student() {
+        cout << "Object Created\n";
+    }
+
+    ~Student() {
+        cout << "Object Destroyed\n";
+    }
+
+    void display() {
+        cout << "Using Smart Pointer\n";
+    }
+};
+
+int main() {
+    unique_ptr<Student> ptr1 = make_unique<Student>();
+    ptr1->display();
+
+    shared_ptr<Student> ptr2 = make_shared<Student>();
+    shared_ptr<Student> ptr3 = ptr2;
+
+    cout << "Shared Pointer Count: " << ptr2.use_count() << endl;
+
+    weak_ptr<Student> ptr4 = ptr2;
+
+    if (auto temp = ptr4.lock()) {
+        temp->display();
+    }
+
+    return 0;
+}
