7a333514c2d5aca13ef86bd02b95324a6b758141
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-28 13:06:02 +0530

    Remove constructors from Demo class
    Removed constructor and move constructor from Demo class.

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -6,16 +6,6 @@ class Demo {
     int* data;
 
 public:
-    Demo(int value) {
-        data = new int(value);
-        cout << "Constructor Called\n";
-    }
-
-    Demo(Demo&& obj) {
-        data = obj.data;
-        obj.data = nullptr;
-        cout << "Move Constructor Called\n";
-    }
 
     Demo& operator=(Demo&& obj) {
         if (this != &obj) {
