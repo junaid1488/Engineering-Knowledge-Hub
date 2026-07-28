af558c615ed2af0fc94ac839aa90ef46a4dbe66a
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-28 13:07:41 +0530

    Remove move semantics example from main
    Removed move semantics demonstration in main function.

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -40,17 +40,7 @@ public:
 };
 
 int main() {
-    Demo obj1(100);
 
-    Demo obj2 = std::move(obj1);
-    obj2.display();
-
-    Demo obj3(200);
-    obj3 = std::move(obj2);
-    obj3.display();
-
-    obj1.display();
-    obj2.display();
 
     return 0;
 }
