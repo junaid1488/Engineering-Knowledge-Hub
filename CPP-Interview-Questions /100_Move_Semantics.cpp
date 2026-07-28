f520d783e43f2e7b11e4c51317befca9b294ebab
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-28 13:11:48 +0530

    Update 100_Move_Semantics.cpp

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -1,13 +1,2 @@
-#include <iostream>
-#include <utility>
-using namespace std;
-
-class Demo {
-    int* data;
-
-
-        cout << "Move Assignment Operator Called\n";
-        return *this;
-    }
 
 
