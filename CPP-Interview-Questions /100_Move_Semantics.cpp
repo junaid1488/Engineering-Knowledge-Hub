6a33840a6db20dd2a80bbf43eda70749f20d9f71
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-28 12:59:39 +0530

    Implement move semantics in Demo class

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -1,3 +1,56 @@
 #include <iostream>
 #include <utility>
 using namespace std;
+
+class Demo {
+    int* data;
+
+public:
+    Demo(int value) {
+        data = new int(value);
+        cout << "Constructor Called\n";
+    }
+
+    Demo(Demo&& obj) {
+        data = obj.data;
+        obj.data = nullptr;
+        cout << "Move Constructor Called\n";
+    }
+
+    Demo& operator=(Demo&& obj) {
+        if (this != &obj) {
+            delete data;
+            data = obj.data;
+            obj.data = nullptr;
+        }
+        cout << "Move Assignment Operator Called\n";
+        return *this;
+    }
+
+    void display() {
+        if (data)
+            cout << "Value: " << *data << endl;
+        else
+            cout << "No Data\n";
+    }
+
+    ~Demo() {
+        delete data;
+    }
+};
+
+int main() {
+    Demo obj1(100);
+
+    Demo obj2 = std::move(obj1);
+    obj2.display();
+
+    Demo obj3(200);
+    obj3 = std::move(obj2);
+    obj3.display();
+
+    obj1.display();
+    obj2.display();
+
+    return 0;
+}
