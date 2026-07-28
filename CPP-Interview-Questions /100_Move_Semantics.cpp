358fbd595bf0ba1ee991a91d57a2402ebfab4b4e
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-28 13:06:13 +0530

    Update 100_Move_Semantics.cpp

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -5,15 +5,7 @@ using namespace std;
 class Demo {
     int* data;
 
-public:
 
-    Demo& operator=(Demo&& obj) {
-        if (this != &obj) {
-            delete data;
-            data = obj.data;
-            obj.data = nullptr;
-
-    ~Demo() {
         delete data;
     }
 
