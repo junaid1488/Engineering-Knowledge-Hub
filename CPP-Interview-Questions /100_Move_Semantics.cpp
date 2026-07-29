97697c069bab3143b951925f9cc692211aec2619
Mohd Juned <akhtaralam9936@gmail.com>
2026-07-29 11:49:20 +0530

    Add comments and structure to move semantics example
    Added comments and structure for move semantics example.

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -1,1 +1,17 @@
+// Author: Mohd Juned
+// Language: C++
+// Topic: Move Semantics
+
+// Include Required Header Files
+// Use Standard Namespace
+
+// Class Definition
+// Constructor
+// Move Constructor
+// Move Assignment Operator
+
+// Main Function
+// Create Object
+// Transfer Resources Using std::move()
+// Display Output
 
