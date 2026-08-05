2c8aa7c9966dddf5bfd0a8fcfda4f554b89fb10b
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-05 19:35:13 +0530

    Update 100_Move_Semantics.cpp

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -1,17 +1,3 @@
 #include <iostream>
 #include <memory>
 using namespace std;
-
-    shared_ptr<Student> ptr2 = make_shared<Student>();
-    shared_ptr<Student> ptr3 = ptr2;
-
-    cout << "Shared Pointer Count: " << ptr2.use_count() << endl;
-
-    weak_ptr<Student> ptr4 = ptr2;
-
-    if (auto temp = ptr4.lock()) {
-        temp->display();
-    }
-
-    return 0;
-}
