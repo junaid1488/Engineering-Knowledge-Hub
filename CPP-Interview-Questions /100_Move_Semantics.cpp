b2b8cc3686cbe0f19df30fc315099c2f6bcf889d
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-05 19:32:24 +0530

    Update 100_Move_Semantics.cpp

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -5,18 +5,7 @@
 // Include Required Header Files
 // Use Standard Namespace
 
-// Smart Pointer Declaration
-// Create Unique Pointer
-// Create Shared Pointer
-// Create Weak Pointer
-// Author: Mohd Juned
-// Language: C++
-// Topic: Smart Pointers
-
-// Include Required Header Files
-// Use Standard Namespace
-
-// Smart Pointer Declaration
+/
 // Create Unique Pointer
 // Create Shared Pointer
 // Create Weak Pointer
