5c2b119a749ce4de49ef7858ee3a55af0940da50
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-14 16:36:50 +0530

    Clean up comments in 100_Move_Semantics.cpp
    Removed unnecessary comments and header files.

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -2,11 +2,6 @@
 // Language: C++
 // Topic: Snapshot Array
 
-// Include Required Header Files
-// Use Standard Namespace
-
-// SnapshotArray Class Definition
-
 // Private Data Members
 // Vector to Store Snapshot Updates
 // Current Snapshot ID Variable
