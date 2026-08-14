85d03dcc24b6f241f10888644422d5ccba36b96e
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-14 16:36:39 +0530

    Clean up comments in 100_Move_Semantics.cpp
    Removed commented-out code for clarity.

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -27,11 +27,6 @@
 // Increment Snapshot ID
 // Return Snapshot Number
 
-// Get Function
-// Perform Binary Search
-// Find Required Snapshot
-// Handle Default Value
-// Return Stored Value
 
 // Main Function
 // Create Object
