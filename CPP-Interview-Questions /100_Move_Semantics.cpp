fb447457ce5a9707cf120ee1d4def36028271a65
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-28 19:31:37 +0530

    Refactor get method to simplify return logic
    Remove unnecessary checks for index bounds in get method.

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -2,9 +2,5 @@
         int idx = upper_bound(
             make_pair(snap_id, INT_MAX)
         ) - updates[index].begin();
-
-        if (idx == 0) return 0;
-
-        return updates[index][idx - 1].second;
     }
 };
