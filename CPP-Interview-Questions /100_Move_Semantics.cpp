0506e78b32205ba88c63251e8a5a1d382adf12ee
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-28 19:30:17 +0530

    Remove constructor from SnapshotArray class
    Removed constructor implementation from SnapshotArray class.

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -2,10 +2,7 @@ class SnapshotArray {
     vector<vector<pair<int, int>>> updates;
     int curSnap;
 
-public:
-    SnapshotArray(int length) {
-        updates.resize(length);
-        curSnap = 0;
+
     }
 
     void set(int index, int val) {
