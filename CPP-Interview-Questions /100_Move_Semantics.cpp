c0cf06f13f7d70a8194570e948845ac64f57d5ff
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-28 19:30:30 +0530

    Refactor set method in 100_Move_Semantics.cpp
    Refactor set method to simplify logic for updating values.

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -4,11 +4,6 @@ class SnapshotArray {
 
 
     }
-
-    void set(int index, int val) {
-        if (!updates[index].empty() && updates[index].back().first == curSnap)
-            updates[index].back().second = val;
-        else
             updates[index].push_back({curSnap, val});
     }
 
