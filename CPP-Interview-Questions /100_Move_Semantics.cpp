2ef9c884576ee65fb5b772f6e5e2159f7714ad31
Mohd Juned <akhtaralam9936@gmail.com>
2026-08-28 19:31:22 +0530

    Optimize get function by simplifying upper_bound usage
    Removed unnecessary calls to begin() and end() in upper_bound.

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -1,7 +1,5 @@
     int get(int index, int snap_id) {
         int idx = upper_bound(
-            updates[index].begin(),
-            updates[index].end(),
             make_pair(snap_id, INT_MAX)
         ) - updates[index].begin();
 
