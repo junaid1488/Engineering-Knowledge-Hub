01a3dda0fe1903a268e054d4534e05be11f9a7e4
Mohd Juned <akhtaralam9936@gmail.com>
2026-09-02 21:11:44 +0530

    Clean up 100_Move_Semantics.cpp
    Remove unnecessary includes and clean up code.

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -1,13 +1,1 @@
 
-#include <iostream>
-#include <unordered_map>
-using namespace std;#include <iostream>
-using namespace std;
-
-int main() {
-    int n = 5;
-
-    if (n <= 2) {
-        cout << n;
-        return 0;
-    }
