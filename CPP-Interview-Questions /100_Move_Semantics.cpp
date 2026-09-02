0698b625c87ccfa2c233e93c91f06d7b1b9cd623
Mohd Juned <akhtaralam9936@gmail.com>
2026-09-02 21:11:16 +0530

    Fix formatting and include directives in 100_Move_Semantics.cpp

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -1,3 +1,13 @@
 
+#include <iostream>
+#include <unordered_map>
+using namespace std;#include <iostream>
+using namespace std;
 
+int main() {
+    int n = 5;
 
+    if (n <= 2) {
+        cout << n;
+        return 0;
+    }
