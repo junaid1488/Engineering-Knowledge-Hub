ee71d724cd6bf7d15ef8f21e214e3d51ac8b3238
Mohd Juned <akhtaralam9936@gmail.com>
2026-09-04 14:00:06 +0530

    Correct grade output statement in main function
    Fixed output statement for grade display.

--- CPP-Interview-Questions /01_Variables.cpp
+++ CPP-Interview-Questions /01_Variables.cpp
@@ -4,7 +4,7 @@ int main() {
     int age = 21;
     double cgpa = 8.5;
     char grade = 'A';
-    cout << "Grade: " << grade << '\n';
+  
     cout << "Placed: " << placed << '\n';
     return 0;
 }
