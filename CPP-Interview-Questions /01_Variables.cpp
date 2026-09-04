ef284b6a2b94e31ce68ce5c2614344eb9bfa324e
Mohd Juned <akhtaralam9936@gmail.com>
2026-09-04 14:06:12 +0530

    Refactor Variables.cpp to include additional variables
    Removed duplicate includes and main function. Added variables for marks, salary, grade, and passed status.

--- CPP-Interview-Questions /01_Variables.cpp
+++ CPP-Interview-Questions /01_Variables.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
-#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float marks = 85.5f;
+    double salary = 50000.75;
+    char grade = 'A';
+    bool passed = true;
+    string name = "Mohd Juned";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+    cout << "Salary: " << salary << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Passed: " << passed << endl;
+
+    return 0;
+}#include <iostream>
 using namespace std;
 
 int main() {
@@ -19,6 +37,7 @@ int main() {
 
     return 0;
 }
+
 #include <iostream>
 using namespace std;
 
