a7ddc770589d8876205c53bcc663cbc588758bc1
Mohd Juned <akhtaralam9936@gmail.com>
2026-09-24 11:51:01 +0530

    Update 100_Move_Semantics.cpp

--- CPP-Interview-Questions /100_Move_Semantics.cpp
+++ CPP-Interview-Questions /100_Move_Semantics.cpp
@@ -12,7 +12,7 @@ int main() {
     cout << "Name: " << name << endl;
     cout << "Age: " << age << endl;
     cout << "Marks: " << marks << endl;
-    cout << "Salary: " << salary << endl;
+   
 
     return 0;
 }
